d7c24b57cb680657942257d7d76a9a52dd9d895b
Manas <shuklamanasofficial@gmail.com>
2023-04-16 08:27:36 +0530

    Minor UI Fix

--- src/components/CardLarge.js
+++ src/components/CardLarge.js
@@ -12,7 +12,7 @@ const CardLarge = ({ value }) => {
 
     return (
         <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 w-full">
-            <div className='h-72 md:h-96 w-1/3 relative'>
+            <div className='h-72 md:h-96 w-full md:w-1/3 relative'>
                 <Image
                     loader={myLoader}
                     src={value?.imageSecondary}
@@ -21,7 +21,7 @@ const CardLarge = ({ value }) => {
                     style={{ objectFit: "cover", objectPosition: "center" }}
                 />
             </div>
-            <div class="flex flex-col justify-between p-4 leading-normal w-2/3">
+            <div class="flex flex-col justify-between p-4 leading-normal w-full md:w-2/3">
                 <h5 class="mb-2 text-3xl font-bold tracking-tight text-stone-800 font-playfair">{value?.title}</h5>
                 <p class="mb-3 font-normal text-gray-700">{truncate(value?.description)}</p>
             </div>
